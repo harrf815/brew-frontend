029a92a7e424a9872ec9d7952a4984eee74e2e05
Harrf Akbar <harrf_akbar@yahoo.com>
2021-03-26 15:41:16 -0700

    working on search

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Map from './component/map/Map'
 import NavContainer from './component/nav/NavContainer';
 
-const baseUrl = `http://localhost:3000/breweries`
+const baseUrl = `http://localhost:3000/breweries/state`
 
 class App extends React.Component {
 
   state = {
-    breweries: []
+    breweries: [], 
+    searchTerm: ''
   }
 
   componentDidMount() {
@@ -16,11 +17,21 @@ class App extends React.Component {
     .then(data => this.setState({breweries: data}))
   }
 
-  onSearch = () => {
+  onSearch = (e) => {
+    // console.log(e.target.value)
+    e.preventDefault()
+    this.setState({ searchTerm: e.target.value})
+  }
+
+  onSearchSubmit(){
 
   }
 
+  //! trying to get this console.log to produce the searhterm
   render () {
+    console.log(this.state.breweries.filter(brew => {
+      brew.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+    }))
     return (
       <div className="App">
         <NavContainer onSearch={this.onSearch}/>
