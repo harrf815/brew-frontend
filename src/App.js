7aacff0148e4c88da4ad7b1b1d55cd12708eba5b
Harrf Akbar <harrf_akbar@yahoo.com>
2021-03-30 11:27:17 -0700

    force commit

--- src/App.js
+++ src/App.js
@@ -51,7 +51,6 @@ const token = localStorage.token
     }
 
 //! this is to set state after login is called on the login page
-
   login = (data) => {
     localStorage.setItem("token", data.jwt);
     this.setState({
