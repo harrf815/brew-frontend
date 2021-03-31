96cd09ee596833096394d9afea3904e12a0ceeec
Harrf Akbar <harrf_akbar@yahoo.com>
2021-03-31 15:34:09 -0700

    minor changes

--- src/component/brewery/BreweryPage.js
+++ src/component/brewery/BreweryPage.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 import { api } from '../../services/Api'
 
 export default class BreweryPage extends Component {
-    state = {
-        brewery: []
-    }
+    state = { brewery: [] }
 
     componentDidMount(){
         let id = parseInt(this.props.match.params.id)
         api.breweries.getBrewery(id).then(brewery => this.setState({brewery: brewery}))
     }
+    
     render() {
         return (
             <div>
