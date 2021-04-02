ff9146cb8488d6cc9d69d3624307c90c07ea6ded
syd333 <sydmiranda@icloud.com>
2021-04-01 20:42:53 -0700

    account page

--- src/component/nav/Account.js
+++ src/component/nav/Account.js
@@ -1,27 +1,26 @@
 import React from "react";
 
+//liked brews could potential be a list of breweries user liked, airbnb sort of
 class Account extends React.Component {
   render() {
     return (
       <div className="account-container">
-        <h2 class="ui header">
-          <i class="settings icon"></i>
-          <div class="content">
-            Account Settings
-            <div class="sub header">Manage your preferences</div>
-          </div>
+        <h2 class="account-head ui header">
+          Account Settings
+          <div class="sub header"></div>
         </h2>
-        <div class="account-dropdown ui secondary vertical menu">
-          <a class="item">Profile</a>
-          <a class="item">Settings</a>
-          <div class="ui dropdown item">
-            <i class="dropdown icon"></i>
-            Display Options
-            <div class="menu">
-              <div class="header">Text Size</div>
-              <a class="item">Small</a>
-              <a class="item">Medium</a>
-              <a class="item">Large</a>
+        <div class="account-grid ui grid">
+          <div class="four wide column">
+            <div class="ui vertical fluid tabular menu">
+              <a class="item">Profile</a>
+              <a class="item">Preferences</a>
+              <a class="item">Liked Brews</a>
+            </div>
+          </div>
+          <div class="eight wide stretched column">
+            <div class="ui segment">
+              brew project will soon be released in your app store! <br></br>
+              update your number to receive the latest info <br></br>
             </div>
           </div>
         </div>
